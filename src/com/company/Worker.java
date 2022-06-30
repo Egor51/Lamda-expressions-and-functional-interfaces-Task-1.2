16c790838ea62096c2c8b3175fdf2a2beda148a6
Egor51 <kubrakov_egor87@mail.ru>
2022-06-30 16:19:20 +0300

    First commit

---
+++ src/com/company/Worker.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+
+    public Worker (OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.errorCallback =  errorCallback;
+        this.callback = callback;
+    }
+
+
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task" +" " +  i +" "+ "is done");
+            if (i == 33){
+                errorCallback.onError("Task"+" " + 33 + " " + "Is not done" );
+            }
+        }
+
+    }
+}
+
+
